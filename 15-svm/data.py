711d067d9a1a26a62a0ae49de77261dc69fc0310
Aaron Schumacher <ajschumacher@gmail.com>
2014-04-20 18:20:23 -0400

    add some data

---
+++ 15-svm/data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pandas as pd
+
+np.random.seed(42)
+x = np.random.uniform(size=1000)
+y = np.random.uniform(size=1000)
+z = ((x-0.5)**2 + (y-0.5)**2) <= (0.5/np.pi)
+x = (x - 0.5) * 0.8
+y = y * 100
+
+d = pd.DataFrame({'feature_1': x,
+                  'feature_2': y,
+                  'label': z})
+d.to_csv('data.csv')
