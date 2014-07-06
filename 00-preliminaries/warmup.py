b79f0747fc42f246b8e23d16b61a48ab62f2fa10
Aaron Schumacher <ajschumacher@gmail.com>
2014-07-06 10:20:26 -0400

    quick fix mistake in python assertions
    The additional "assert"s were redundant (np.testing.assert_array_equal
    doesn't return True)

--- 00-preliminaries/warmup.py
+++ 00-preliminaries/warmup.py
@@ -21,7 +21,7 @@ x, y = (np.random.randn(100) for _ in range(2))
 def equal(a, b):
   np.testing.assert_array_equal(a, b)
 
-assert(equal(my_mean(x), np.mean(x)))
-assert(equal(my_var(x), np.var(x)))
-assert(equal(my_cov(x, y), np.cov(x, y)))
-assert(equal(my_cor(x, y), np.corrcoef(x, y)))
+equal(my_mean(x), np.mean(x))
+equal(my_var(x), np.var(x))
+equal(my_cov(x, y), np.cov(x, y))
+equal(my_cor(x, y), np.corrcoef(x, y))
